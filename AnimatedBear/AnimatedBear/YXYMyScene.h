8ec13b4193d56c2d4a9f329626856966c711d059
yulingtianxia <yulingtianxia@gmail.com>
2014-04-01 21:09:37 +0800

    加入熊熊

---
+++ AnimatedBear/AnimatedBear/YXYMyScene.h
@@ -0,0 +1,13 @@
+//
+//  YXYMyScene.h
+//  AnimatedBear
+//
+
+//  Copyright (c) 2014年 杨萧玉. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface YXYMyScene : SKScene
+
+@end
---
+++ AnimatedBear/AnimatedBearTests/AnimatedBearTests.m
@@ -0,0 +1,34 @@
+//
+//  AnimatedBearTests.m
+//  AnimatedBearTests
+//
+//  Created by 杨萧玉 on 14-4-1.
+//  Copyright (c) 2014年 杨萧玉. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnimatedBearTests : XCTestCase
+
+@end
+
+@implementation AnimatedBearTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
